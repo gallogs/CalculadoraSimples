9ea312734eeceefd2c4f6d4272376f8f532bec67
Matheus Gallo <gallomatheus.ti@gmail.com>
2023-10-01 14:32:47 -0300

    Atualização do código.

--- CalculadoraSimples/Program.cs
+++ CalculadoraSimples/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Calculadora com +, -, *, /.
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("====================Calculadora=====================");
+
+        Console.WriteLine("Escolha: 1: + | 2: - | 3: * | 4: %");
+        int? escolha = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o primeiro numero: ");
+        int? numero1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o segundo numero: ");
+        int? numero2 = int.Parse(Console.ReadLine());
+
+        switch (escolha)
+        {
+            case 1:
+                Console.WriteLine($"Resultado: {numero1 + numero2}");
+                break;
+            case 2:
+                Console.WriteLine($"Resultado: {numero1 - numero2}");
+                break;
+            case 3:
+                Console.WriteLine($"Resultado: {numero1 * numero2}");
+                break;
+            case 4:
+                Console.WriteLine($"Resultado: {numero1 / numero2}");
+                break;
+        }
+    }
+}
